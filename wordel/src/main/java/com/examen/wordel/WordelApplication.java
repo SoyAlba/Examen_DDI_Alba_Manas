c6c66ff8cc29711c00036040a193b35910e23764
SoyAlba <albamanas2003@gmail.com>
2023-02-14 18:55:10 +0100

    sigue descardando el index

--- wordel/src/main/java/com/examen/wordel/WordelApplication.java
+++ wordel/src/main/java/com/examen/wordel/WordelApplication.java
@@ -2,13 +2,13 @@ package com.examen.wordel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.examen.wordel.*" })
 public class WordelApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(WordelApplication.class, args);
 	}
- }
+
+}
